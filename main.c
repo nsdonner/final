82d5aaeca6e1fbe63559dec004a28ccd40c8aeea
Donner Krieger <nsdonner@gmail.com>
2022-11-14 00:20:05 +0300

    Всё вынесено в функции Готова обработка аргументов Доделать параметр -m
    Разобраться с cmake
    Укрошательства и допилы
    Бесконечность не предел!

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void fillIt(short data[13][32][24][60]) {
@@ -59,19 +60,21 @@ short maxTemp(short data[13][32][24][60]) {
     }
     return max;
 }
+
 short maxTempinMonth(short data[13][32][24][60], int mon) {
     short max = -100;
-        for (int d = 0; d < 32; d++) {
-            for (int h = 0; h < 24; h++) {
-                for (int m = 0; m < 60; m++) {
-                    if ((data[mon][d][h][m] > max) && (data[mon][d][h][m] < 100)) {
-                        max = data[mon][d][h][m];
-                    }
+    for (int d = 0; d < 32; d++) {
+        for (int h = 0; h < 24; h++) {
+            for (int m = 0; m < 60; m++) {
+                if ((data[mon][d][h][m] > max) && (data[mon][d][h][m] < 100)) {
+                    max = data[mon][d][h][m];
                 }
             }
         }
+    }
     return max;
 }
+
 short medTemp(short data[13][32][24][60]) {
     int med = 0;
     int count = 0;
@@ -94,30 +97,30 @@ short medTemp(short data[13][32][24][60]) {
 short medTempinMonth(short data[13][32][24][60], int mon) {
     int med = 0;
     int count = 0;
-        for (int d = 0; d < 32; d++) {
-            for (int h = 0; h < 24; h++) {
-                for (int m = 0; m < 60; m++) {
-                    if (data[mon][d][h][m] < 100) {
-                        med = med + data[mon][d][h][m];
-                        count++;
-                    }
+    for (int d = 0; d < 32; d++) {
+        for (int h = 0; h < 24; h++) {
+            for (int m = 0; m < 60; m++) {
+                if (data[mon][d][h][m] < 100) {
+                    med = med + data[mon][d][h][m];
+                    count++;
                 }
             }
         }
+    }
 
 
     return med / count;
 }
 
-int tempMath(char filename[], int monthNum){
+int tempMath(char filename[], int monthNum) {
 
     FILE *f;
     int a = 0;
     char s[10000];
     short data[13][32][24][60] = {0};
     f = fopen(filename, "r");
-    if (f == NULL){
-        printf("\n Can't open file %s\n",filename);
+    if (f == NULL) {
+        printf("\n Can't open file %s\n", filename);
         return 1;
     }
     int year, month, day, hour, minute;
@@ -140,11 +143,16 @@ int tempMath(char filename[], int monthNum){
     fclose(f);
 
 
-
-    for (int i=1;i<13;i++){
-        printf("Month %d median temp is %hi\n",i, medTempinMonth(data,i));
-        printf("Mont %d minimum temp is %hi\n",i, minTempinMonth(data,i));
-        printf("Mont %d maximum temp is %hi\n",i, maxTempinMonth(data,i));
+    if (monthNum == 0) {
+        for (int i = 1; i < 13; i++) {
+            printf("Month %d median temp is %hi\n", i, medTempinMonth(data, i));
+            printf("Mont %d minimum temp is %hi\n", i, minTempinMonth(data, i));
+            printf("Mont %d maximum temp is %hi\n", i, maxTempinMonth(data, i));
+        }
+    } else {
+        printf("Month %d median temp is %hi\n", monthNum, medTempinMonth(data, monthNum));
+        printf("Mont %d minimum temp is %hi\n", monthNum, minTempinMonth(data, monthNum));
+        printf("Mont %d maximum temp is %hi\n", monthNum, maxTempinMonth(data, monthNum));
     }
 
     printf("%d median temp is %hi\n", year, medTemp(data));
@@ -158,7 +166,7 @@ int tempMath(char filename[], int monthNum){
 
 int main(int argc, char **argv) {
 
-    if ((argc > 5)||(argc == 4 )) {
+    if ((argc > 5) || (argc == 4)) {
         printf("Error: found wrong arguments.");
         return (1);
     }
@@ -167,7 +175,7 @@ int main(int argc, char **argv) {
         return (1);
     }
     if (argc == 2) {
-        if (!strcmp(argv[1],"-h")){
+        if (!strcmp(argv[1], "-h")) {
             printf("-h for this help message\n");
             printf("-f for filename\n");
             printf("-m for month number\n");
@@ -179,13 +187,22 @@ int main(int argc, char **argv) {
         } else printf("Error: type -h for help");
         return (1);
     }
-    if (argc == 3){
-        if (!strcmp(argv[1],"-f")) {
-            return(tempMath(argv[2],0));
+    if (argc == 3) {
+        if (!strcmp(argv[1], "-f")) {
+            return (tempMath(argv[2], 0));
+        } else {
+            printf("Error: type -h for help");
+            return (1);
         }
     }
-
-
+    if (argc == 5) {
+        if ((!strcmp(argv[1], "-f"))&&(!strcmp(argv[3], "-m"))&&(atoi(argv[4])>0)&&(atoi(argv[4])<13)) {
+            return (tempMath(argv[2], atoi(argv[4])));
+        } else {
+            printf("Error: type -h for help");
+            return (1);
+        }
+    }
 
 
     return 0;
